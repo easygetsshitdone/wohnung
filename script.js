a0790a167b441716f0b89d5bc86812c204d42ce0
Your Name <izkfrmx@gmail.com>
2025-08-21 15:27:24 +0200

    Meine Änderungen

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 function checkCode() {
-    // Hier kannst du den Geheimcode festlegen
-    const correctCode = "danke123"; 
-    
-    // Eingabe des Nutzers abrufen
+    const correctCode = "danke123";
     const enteredCode = document.getElementById('codeInput').value;
-    
     const overlay = document.querySelector('.overlay');
     
-    // Überprüfen, ob der Code korrekt ist
     if (enteredCode === correctCode) {
-        overlay.classList.add('hidden'); // Blendet das Overlay aus
+        overlay.classList.add('hidden');
         const contactInfo = document.getElementById('contactInfo');
-        contactInfo.style.display = 'block'; // Zeigt Kontaktdaten
+        contactInfo.style.display = 'block'; 
     } else {
-        // Die Meldung wurde entfernt, es passiert einfach nichts.
+        alert("Falscher Code. Bitte versuchen Sie es erneut.");
     }
 }
